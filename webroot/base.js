0f61f4f180b767653614f33355745f4723990fb4
Daniel <desiradaniel2007@hotmail.com>
2014-07-30 21:35:43 +0200

    Reorganized some stuff

--- webroot/base.js
+++ webroot/base.js
@@ -2,16 +2,23 @@
 	
 	'use strict';
 
-	function submit(event) {
+	function submit() {
 		$.post('qr_proc.php', {
 			'qr': $('[name="qr"]').val(),
 			'submit': 1
 		}, response);
-		event.preventDefault();
+		return false;
 	}
 
 	function response(data) {
-
+		if (data === '1') {
+			var score = localStorage.getItem('score');
+			if (score) {
+				localStorage.setItem('score', score + 1);
+			} else {
+				localStorage.setItem('score', 1);
+			}
+		}
 	}
 
 	$('#qr_form').submit(submit);
