9fe0f096bd18b538a4ec14ee766e0646b1ce4d8a
Daniel <desiradaniel2007@hotmail.com>
2014-08-11 00:23:03 +0200

    Displaying fun facts

--- webroot/base.js
+++ webroot/base.js
@@ -31,6 +31,7 @@
 			$('#non_occuring_qr').modal('show');
 		} else if (resObj.qr_remaining) {
 			$('#qr_remaining').text(resObj.qr_remaining);
+			$('#funfact').text(resObj.funfact);
 		} else if (resObj.token_code !== -1) {
 			$('#token_code').text(resObj.token_code);
 			$('#token_code_modal').modal('show');
--- webroot/server.php
+++ webroot/server.php
@@ -19,26 +19,27 @@ if (array_search($qr, $_SESSION['qr_codes']) !== FALSE) {
 	die();
 }
 
-$statement = $db->prepare('SELECT COUNT(*), funfact FROM stand WHERE qrcode=?');
+$statement = $db->prepare('SELECT COUNT(*) AS qr_count, funfact FROM stand WHERE qrcode=?');
 $statement->bindValue(1, $qr, PDO::PARAM_STR);
 $statement->execute();
+$qr_row = $statement->fetch(PDO::FETCH_ASSOC);
 
-if (! $statement->fetchColumn(0)) {
+if (! $qr_row['qr_count']) {
 	?>{"non_occuring":1}<?php
 	die();
 }
 
 $_SESSION['qr_codes'][] = $qr;
 
-$qr_remaining = 10 - count($_SESSION['qr_codes']);var_dump($statement->fetchColumn(1));
+$qr_remaining = 10 - count($_SESSION['qr_codes']);//var_dump($statement->fetchColumn(1));
 
 ?>
-{"qr_remaining":<?php echo $qr_remaining; ?>,"funfact":"<?php echo $statement->fetchColumn(1); ?>"}
+{"qr_remaining":<?php echo $qr_remaining; ?>,"funfact":"<?php echo $qr_row['funfact']; ?>"}
 <?php
 
-if (! $qr_remaining) {
+if ($qr_remaining < 1) {
 	$current_code_id = $db->query('SELECT code_id FROM current_code', PDO::FETCH_COLUMN, 0);
-	$current_code = $db->query('SELECT code FROM codes WHERE code_id=' . $current_code_id, PDO::FETCH_COLUMN, 0);
+	$current_code = $db->query('SELECT code FROM codes WHERE code_id=' . $current_code_id, \PDO::FETCH_ASSOC, 0);
 	?>{"token_code":<?php echo $current_code !== 5 ? $current_code : -1; ?>}<?php
 	$current_code_id++;
 	$db->query('UPDATE current_code SET code_id=' . $current_code_id);
